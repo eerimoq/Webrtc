744861bd0b3ddda353fbc33ae35060c0649f042c
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-10 20:32:58 +0200

    datachannel moblin-0.31.0.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(name: "DataChannel", dependencies: ["libdatachannel"]),
         .binaryTarget(name: "libdatachannel",
-                      url: "https://github.com/eerimoq/xcframeworks/releases/download/libdatachannel-0.30.0/libdatachannel.xcframework.zip",
-                      checksum: "d7589584a38c97da1525acf6b830877ceee95246bf27db3644e7e2e65086ce28"),
+                      url: "https://github.com/eerimoq/xcframeworks/releases/download/libdatachannel-0.31.0/libdatachannel.xcframework.zip",
+                      checksum: "961ffc16a6028ceb1b833392be64502d01b928cc1bfb754a1494b6ae8226ab39"),
     ]
 )
